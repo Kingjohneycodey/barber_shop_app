42a0f9e020ade8740cb7f24554532dd592c6175b
kingjohneycodey <kvngjohnny10@gmail.com>
2025-08-28 19:34:29 +0100

    project setup

---
+++ lib/core/gen/fonts.gen.dart
@@ -0,0 +1,25 @@
+// dart format width=80
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: deprecated_member_use,directives_ordering,implicit_dynamic_list_literal,unnecessary_import
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: AksharBold
+  static const String aksharBold = 'AksharBold';
+
+  /// Font family: AksharLight
+  static const String aksharLight = 'AksharLight';
+
+  /// Font family: AksharMedium
+  static const String aksharMedium = 'AksharMedium';
+
+  /// Font family: AksharRegular
+  static const String aksharRegular = 'AksharRegular';
+}
