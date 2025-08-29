cae07fd98bd624ab3f43125033702789e3e03fc4
kingjohneycodey <kvngjohnny10@gmail.com>
2025-08-29 07:31:26 +0100

    worked on the profile page

--- lib/features/home_feature/presentation/widgets/tabs/profile_tab.dart
+++ lib/features/home_feature/presentation/widgets/tabs/profile_tab.dart
@@ -1,15 +1,86 @@
-import 'package:flutter/material.dart';
+import 'package:barber_shop/core/gen/assets.gen.dart';
+import 'package:barber_shop/core/gen/fonts.gen.dart';
+import 'package:barber_shop/core/theme/colors.dart';
+import 'package:barber_shop/core/theme/dimens.dart';
+import 'package:barber_shop/core/widgets/app_elevated_button.dart';
+import 'package:barber_shop/core/widgets/app_light_text.dart';
+import 'package:barber_shop/core/widgets/app_list_tile.dart';
+import 'package:barber_shop/core/widgets/user_profile_image_widget.dart';
+import 'package:flutter/cupertino.dart';
 
-class ProfileTab extends StatefulWidget {
+class ProfileTab extends StatelessWidget {
   const ProfileTab({super.key});
 
   @override
-  State<ProfileTab> createState() => _ProfileTabState();
-}
-
-class _ProfileTabState extends State<ProfileTab> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        spacing: Dimens.largePadding,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          UserProfileImageWidget(width: 150, height: 150),
+          Column(
+            spacing: Dimens.padding,
+            children: [
+              Text(
+                'Vanessa Lennox',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontFamily: FontFamily.aksharBold,
+                ),
+              ),
+              AppLightText('VanessaLennox@gmail.com'),
+            ],
+          ),
+          AppElevatedButton(title: 'Edit profile', onPressed: () {}),
+          AppListTile(
+            onTap: () {},
+            title: 'Payment method',
+            leadingIconPath: Assets.icons.cardPos,
+          ),
+          AppListTile(
+            onTap: () {},
+            title: 'Language',
+            leadingIconPath: Assets.icons.language,
+          ),
+          AppListTile(
+            onTap: () {},
+            title: 'Notification',
+            leadingIconPath: Assets.icons.notification,
+            trailing: Transform.scale(
+              scale: 0.7,
+              child: CupertinoSwitch(
+                value: true,
+                onChanged: (final value) {},
+                activeTrackColor: AppColors.primaryColor,
+              ),
+            ),
+          ),
+          AppListTile(
+            onTap: () {},
+            title: 'Dark mode',
+            leadingIconPath: Assets.icons.moon,
+            trailing: Transform.scale(
+              scale: 0.7,
+              child: CupertinoSwitch(
+                value: false,
+                onChanged: (final value) {},
+                activeTrackColor: AppColors.primaryColor,
+              ),
+            ),
+          ),
+          AppListTile(
+            onTap: () {},
+            title: 'Help and support',
+            leadingIconPath: Assets.icons.info,
+          ),
+          AppListTile(
+            onTap: () {},
+            title: 'Log out',
+            leadingIconPath: Assets.icons.logout,
+          ),
+        ],
+      ),
+    );
   }
 }
